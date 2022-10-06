3ac5cc3a5587db2eda4956f5093dee622ee4ab9a
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-06 08:40:05 -0500

    Created app.html and started styles

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import {createApp} from "vue";
+
+const app = createApp({
+    template: `
+    
+    <div class="gridcont" > 
+
+    <div class="menu"></div>
+    </div>  
+    
+    
+    
+    `
+})
