a1178e5342e388077c6717e0a81951e8a3319a72
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-07 10:09:21 -0500

    What did that even do??

--- src/app.js
+++ src/app.js
@@ -1,5 +1,25 @@
-export default {
-        template: `
-            <div>WHAT IS HAPPENING</div> 
-        `
-}
+
+import menu_item from "./comp/menu_item";
+
+//Todays Date: 10/7/22
+
+
+export default{
+    Data() {
+        return {page: "home"}
+    },
+
+    template: `
+    
+    <div class="gront"> 
+
+    <div id="menu">
+    <menu_item onClick="page = 'home'">Home</menu_item>
+    <menu_item onClick"page = 'img'">images</page_bar> 
+    </div>
+
+
+    </div>  
+    `
+};
+
