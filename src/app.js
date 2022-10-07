03948c51150501f4387fc89df8b9d5cf857578be
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-07 13:19:04 -0500

    more test stuff

--- src/app.js
+++ src/app.js
@@ -1,21 +1,7 @@
+import {createApp} from "vue";
 
-import menuItem from "./comp/menuItem.js";
+const app = createApp({
+    template: `<div>epic</div>`
+});
 
-//Todays Date: 10/7/22
-
-
-export default{
-    template: `
-    
-    <div class="gront"> 
-
-    <div id="menu">
-    <menuItem ></menuItem>
- 
-    </div>
-
-
-    </div>  
-    `
-};
-
+app.mount("#app");
