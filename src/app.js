b4d7bede1bc44b81a5d6e0957c71602953f318e3
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-07 09:42:55 -0500

    PLEASE WORK I BEG OF YOU

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
 import { createApp } from "vue";
+import menu_item from "./comp/menu_item";
+
+//Todays Date: 10/7/22
+
 
 const app = createApp({
-    template: `<div>hello world!!!</div>`
+    Data(){
+        return {page: "home"}
+    },
+    template: `
+    
+    <div class="gront"> 
+
+    <div id="menu">
+    <menu_item onClick="page = 'home'">Home</menu_item>
+    <menu_item onClick"page = 'img'">images</menu_item> 
+    </div>
+
+
+    </div>  
+    `
 });
 
 app.mount("#app");
