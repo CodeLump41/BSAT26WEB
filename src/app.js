ce6765edb2c1b00949b216add42c7f1a19196e1c
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-07 09:22:18 -0500

    Debugging

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import { createApp } from "vue";
-import menu_item from "./comp/menu_item";
+// import menu_item from "./comp/menu_item";
 
 //Todays Date: 10/7/22
 
@@ -10,15 +10,7 @@ const app = createApp({
     },
     template: `
     
-    <div class="gront"> 
-
-    <div id="menu">
-    <menu_item onClick="page = 'home'">Home</menu_item>
-    <menu_item onClick"page = 'img'">images</menu_item> 
-    </div>
-
-
-    </div>  
+   <p>DEBUGGING</p>  
     `
 });
 
