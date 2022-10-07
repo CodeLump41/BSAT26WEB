accfafac7668f4892fae9af7c6901c781f21cd65
CodeLump41 <114756610+CodeLump41@users.noreply.github.com>
2022-10-07 09:41:36 -0500

    partwinker

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 import { createApp } from "vue";
-// import menu_item from "./comp/menu_item";
-
-//Todays Date: 10/7/22
-
-/*
- 
- */
 
 const app = createApp({
-    template: `
-   <p>DEBUGGING</p>  
-    `
+    template: `<div>hello world!!!</div>`
 });
 
 app.mount("#app");
